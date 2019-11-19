50d0b1ec2322645d18d8df7c6d38123e93f4f776
Rohan Kumar <108krohan@users.noreply.github.com>
2019-11-19 21:41:43 +0530

    add noble-integer.cpp
    https://www.interviewbit.com/problems/noble-integer/

---
+++ arrays/noble-integer.cpp
@@ -0,0 +1,25 @@
+/*
+Given an integer array, find if an integer p exists in the array such that the number of integers greater than p in the array equals to p
+If such an integer is found return 1 else return -1.
+*/
+
+int Solution::solve(vector<int> &A) {
+    int n = A.size(), ans = -1, i = 0;
+    
+    sort(A.begin(), A.end());
+    
+    while(i < n){
+        while(i+1 < n && A[i] == A[i+1]){
+            i++;
+        }
+        
+        if(n - (i+1) == A[i]){
+            ans = 1;
+            break;
+        }
+        
+        i++;
+    }
+    
+    return ans;
+}
