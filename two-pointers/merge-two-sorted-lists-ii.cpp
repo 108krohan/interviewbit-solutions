d086a3f1e2c43298b8cd33d7ab4b16a077f8302d
Rohan Kumar <108krohan@users.noreply.github.com>
2019-11-27 15:27:54 +0530

    add merge-two-sorted-lists-ii.cpp
    https://www.interviewbit.com/problems/merge-two-sorted-lists-ii/

---
+++ two-pointers/merge-two-sorted-lists-ii.cpp
@@ -0,0 +1,47 @@
+/*
+
+Given two sorted integer arrays A and B, merge B into A as one sorted array.
+
+    Note: You have to modify the array A to contain the merge of A and B. Do not output anything in your code.
+    TIP: C users, please malloc the result into a new array and return the result. 
+
+If the number of elements initialized in A and B are m and n respectively, the resulting size of array A after your code is executed should be m + n
+
+Example :
+
+Input : 
+         A : [1 5 8]
+         B : [6 9]
+
+Modified A : [1 5 6 8 9]
+
+*/
+
+void Solution::merge(vector<int> &A, vector<int> &B) {
+    int B_inc = 0; 
+    int A_inc = 0;
+    vector<int> c; 
+    while(A_inc < A.size() && B_inc < B.size() ) {
+        if(A.at(A_inc) > B.at(B_inc)) {
+            
+            c.push_back(B.at(B_inc));
+            B_inc++;
+        }
+        else {
+            c.push_back(A.at(A_inc));
+            A_inc++;
+        }
+    }
+    
+    while(A_inc < A.size()) {
+        c.push_back(A.at(A_inc));
+        A_inc++;
+    }
+    
+    while(B_inc < B.size()) {
+        c.push_back(B.at(B_inc));
+        B_inc++;
+    }
+    
+    A = c;
+}
