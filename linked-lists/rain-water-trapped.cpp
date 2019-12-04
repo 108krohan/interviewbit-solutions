486454c58c00fc29c3e7aac4a9621fb9cfd99073
Rohan Kumar <108krohan@users.noreply.github.com>
2019-12-04 20:46:09 +0530

    add rain-water-trapped.cpp
    https://www.interviewbit.com/problems/rain-water-trapped/

---
+++ linked-lists/rain-water-trapped.cpp
@@ -0,0 +1,51 @@
+/*
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+
+
+Input Format
+
+The only argument given is integer array A.
+
+Output Format
+
+Return the total water it is able to trap after raining..
+
+For Example
+
+Input 1:
+    A = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output 1:
+    6
+Explaination 1: <img src="http://i.imgur.com/0qkUFco.png">
+    
+    In this case, 6 units of rain water (blue section) are being trapped.
+
+*/
+
+int Solution::trap(const vector<int> &a) {
+    int i = 0;
+    stack<int>stack;
+    int water = 0;
+    
+    while(i < a.size()){
+        while(!stack.empty() && a[i] > a[stack.top()]){
+            int t = stack.top();
+            stack.pop();
+            int ele;
+            int dist;
+            
+            if(!stack.empty()){
+                ele = min(a[i], a[stack.top()]) - a[t];
+                dist = i-1-stack.top();
+                water += ele*dist;
+            }
+        }
+        stack.push(i); //logging indices
+        i++;
+    }
+    
+    return water;
+}
+
